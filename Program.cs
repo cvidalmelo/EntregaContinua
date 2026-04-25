2ec3616e74f5347541088e4d3b9965855cc9592b
cvidal <cvidal@arssenasa.gob.do>
2026-04-25 16:21:57 -0400

    Practica Final DevOps

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola Mundo desde .NET en Docker, github actions y Render!!");
+app.MapGet("/", () => "Hola Mundo desde .NET en Docker, Practica final!");
 
 app.Run();
