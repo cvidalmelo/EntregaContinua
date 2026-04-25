ca5182f5ca36aba3424500d869749934317be6a0
cvidal <cvidal@arssenasa.gob.do>
2026-04-24 23:40:06 -0400

    Update program a ultima version

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola Mundo desde .NET en Docker y github actionS");
+app.MapGet("/", () => "Hola Mundo desde .NET en Docker, github actions y Render");
 
 app.Run();
