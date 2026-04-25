93d5a89186e0821025f95f51cf1337e040d6485e
cvidal <cvidal@arssenasa.gob.do>
2026-04-24 20:56:10 -0400

    fixe secrets 4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola Mundo desde .NET en Docker y github action");
+app.MapGet("/", () => "Hola Mundo desde .NET en Docker y github actionS");
 
 app.Run();
